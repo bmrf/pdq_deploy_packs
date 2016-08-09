1b39d747d770c10fd16602560cf534f8c3f5ea4e
Vocatus Gate <vocatus.gate@gmail.com>
2016-08-09 08:29:10 -0400

    v1.0.1

--- repository/mozilla/firefox/x86/Firefox.bat
+++ repository/mozilla/firefox/x86/Firefox.bat
@@ -1,7 +1,7 @@
 :: Purpose:       Installs a package
 :: Requirements:  Run this script with Administrator rights
 :: Author:        vocatus on reddit.com/r/sysadmin ( vocatus.gate@gmail.com ) // PGP key ID: 0x07d1490f82a211a2
-:: History:       1.0.1 * Modify installation flags to use configuration.ini. Thanks to /u/Doraemon2600
+:: History:       1.0.1 * Expand Desktop shortcut deletion mask to sweep all subdirectories under the base user profile directory
 ::                1.0.0 + Initial write
 
 
@@ -10,7 +10,7 @@
 ::::::::::
 @echo off
 set SCRIPT_VERSION=1.0.1
-set SCRIPT_UPDATED=2015-03-03
+set SCRIPT_UPDATED=2017-08-09
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
@@ -18,6 +18,7 @@ set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
 :: This is useful if we start from a network share; converts CWD to a drive letter
 pushd "%~dp0"
 cls
+
 
 
 :::::::::::::::
@@ -28,8 +29,9 @@ set LOGPATH=%SystemDrive%\Logs
 set LOGFILE=
 
 :: Package to install. Do not use trailing slashes (\)
-set LOCATION=
-set BINARY=Mozilla Firefox x86.exe
+::if     exist "%ProgramFiles(x86)%" set BINARY="Firefox Setup 48.0.0 x64.exe"
+::if not exist "%ProgramFiles(x86)%" set BINARY="Firefox Setup 48.0.0 x86.exe"
+set BINARY="Mozilla Firefox.exe"
 set FLAGS=/INI="%CD%\configuration.ini"
 
 :: Create the log directory if it doesn't exist
@@ -46,21 +48,17 @@ wmic process where name="firefox.exe" call terminate 2>NUL
 :: Install the package from the local folder (if all files are in the same directory)
 "%BINARY%" %FLAGS%
 
-:: Install 32-bit customisations
-if exist "%ProgramFiles%\Mozilla Firefox\" copy /Y "%~dp0override.ini" "%ProgramFiles%\Mozilla Firefox\browser\"
-if exist "%ProgramFiles%\Mozilla Firefox\" copy /Y "%~dp0firefox-custom-settings.js" "%ProgramFiles%\Mozilla Firefox\"
-if exist "%ProgramFiles%\Mozilla Firefox\" copy /Y "%~dp0local-settings.js" "%ProgramFiles%\Mozilla Firefox\defaults\pref"
+:: Install customizations, built with CCK2
+if exist "%ProgramFiles(x86)%\Mozilla Firefox\" xcopy /s /e /y "autoconfig\*" "%ProgramFiles(x86)%\Mozilla Firefox"
+if exist "%ProgramFiles%\Mozilla Firefox\" xcopy /s /e /y "autoconfig\*" "%ProgramFiles(x86)%\Mozilla Firefox"
 
-:: Install 64-bit customisations
-if exist "%ProgramFiles(x86)%\Mozilla Firefox\" copy /Y "%~dp0override.ini" "%ProgramFiles(x86)%\Mozilla Firefox\browser\"
-if exist "%ProgramFiles(x86)%\Mozilla Firefox\" copy /Y "%~dp0firefox-custom-settings.js" "%ProgramFiles(x86)%\Mozilla Firefox\"
-if exist "%ProgramFiles(x86)%\Mozilla Firefox\" copy /Y "%~dp0local-settings.js" "%ProgramFiles(x86)%\Mozilla Firefox\defaults\pref"
+:: Remove desktop icons
+REM if exist "%allusersprofile%\Desktop\Mozilla Firefox.lnk" del "%allusersprofile%\Desktop\Mozilla Firefox.lnk" /S
+REM if exist "%public%\Desktop\Mozilla Firefox.lnk" del "%public%\Desktop\Mozilla Firefox.lnk"
+REM if exist "%SystemDrive%\users\default\Desktop\Mozilla Firefox.lnk" del "%SystemDrive%\users\default\Desktop\Mozilla Firefox.lnk"
 
-:: Remove Firefox Desktop Icon - Windows XP
-if exist "%allusersprofile%\Desktop\Mozilla Firefox.lnk" del "%allusersprofile%\Desktop\Mozilla Firefox.lnk" /S
-
-:: Remove Firefox Desktop Icon - Windows 7
-if exist "%public%\Desktop\Mozilla Firefox.lnk" del "%public%\Desktop\Mozilla Firefox.lnk"
+:: Lets just amp this up and catch ANYWHERE it might drop a shortcut 
+del /f /s "%SystemDrive%\Users\*Mozilla Firefox.lnk" 2>nul
 
 :: Pop back to original directory. This isn't necessary in stand-alone runs of the script, but is needed when being called from another script
 popd
