18b31f55d544fd5e956281344d8ffe57ad541692
Vocatus Gate <vocatus.gate@gmail.com>
2016-08-09 08:29:50 -0400

    Delete local-settings.js

--- repository/mozilla/firefox/x86/local-settings.js
+++
@@ -1,6 +0,0 @@
-// This is a pointer file, it tells the Firefox installer what config file to use.
-// Edit the file specified below (in this case, "firefox-custom-settings.js" to customize your installation.
-
-// Don't obscure config file name
-pref("general.config.obscure_value", 0);
-pref("general.config.filename", "firefox-custom-settings.js");
