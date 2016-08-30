fbf974797e12414902606a3c3c835e3b50df26ed
Vocatus Gate <vocatus.gate@gmail.com>
2016-08-30 14:49:16 -0300

    add --preserve-shortcuts CLI switch

--- repository/notepad/x86/npp.Installer.bat
+++ repository/notepad/x86/npp.Installer.bat
@@ -1,15 +1,16 @@
 :: Purpose:       Installs a package
-:: Requirements:  Run this script with Administrator rights
+:: Requirements:  Run this script with Administrator rights. Use the --preserve-shortcuts CLI switch to prevent the script from deleting Notepad++'s Start Menu shortcuts. Thanks to github:sn3ak
 :: Author:        vocatus on reddit.com/r/sysadmin ( vocatus.gate@gmail.com ) // PGP key ID: 0x07d1490f82a211a2
-:: History:       1.0.0 + Initial write
+:: History:       1.1.0 + Add --preserve-shortcuts switch and associated PRESERVE_SHORTCUTS variable. Use this to prevent script from deleting Notepad++'s Start Menu shortcuts.
+::                1.0.0 + Initial write
 
 
 ::::::::::
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set SCRIPT_VERSION=1.0.0
-set SCRIPT_UPDATED=2015-04-15
+set SCRIPT_VERSION=1.1.0
+set SCRIPT_UPDATED=2016-08-30
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
@@ -17,6 +18,10 @@ set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
 :: This is useful if we start from a network share; converts CWD to a drive letter
 pushd "%~dp0"
 cls
+
+:: Check for command-line argument
+set PRESERVE_SHORTCUTS=no
+for %%i in (%*) do ( if /i %%i==--preserve-acrotray set PRESERVE_SHORTCUTS=yes )
 
 
 :::::::::::::::
@@ -27,7 +32,6 @@ set LOGPATH=%SystemDrive%\Logs
 set LOGFILE=
 
 :: Package to install. Do not use trailing slashes (\)
-set LOCATION=
 set BINARY=npp.6.9.2.Installer.exe
 set FLAGS=/S
 
@@ -58,7 +62,9 @@ del /f /q "%SystemDrive%\Program Files\Notepad++\plugins\DSpellCheck.dll" 2>NUL
 del /f /q "%SystemDrive%\Program Files (x86)\Notepad++\plugins\DSpellCheck.dll" 2>NUL
 
 :: Delete the Start Menu directory
-if exist "%ALLUSERSPROFILE%\Start Menu\Programs\Notepad++" rmdir /s /q "%ALLUSERSPROFILE%\Start Menu\Programs\Notepad++"
+if %PRESERVE_SHORTCUTS%==no (
+	if exist "%ALLUSERSPROFILE%\Start Menu\Programs\Notepad++" rmdir /s /q "%ALLUSERSPROFILE%\Start Menu\Programs\Notepad++" 
+)
 
 :: Pop back to original directory. This isn't necessary in stand-alone runs of the script, but is needed when being called from another script
 popd
