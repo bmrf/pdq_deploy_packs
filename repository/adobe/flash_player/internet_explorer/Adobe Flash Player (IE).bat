a5ebf2a1da95ad813c1c52bd8fb21385bdfc97a1
Vocatus Gate <vocatus.gate@gmail.com>
2016-09-14 09:06:53 -0300

    increment Flash version number

--- repository/adobe/flash_player/internet_explorer/Adobe Flash Player (IE).bat
+++ repository/adobe/flash_player/internet_explorer/Adobe Flash Player (IE).bat
@@ -26,7 +26,7 @@ set LOGPATH=%SystemDrive%\Logs
 set LOGFILE=%COMPUTERNAME%_Adobe_Flash_IE_install.log
 
 :: Package to install. Do not use trailing slashes (\)
-set BINARY=install_flash_player_22_active_x.msi
+set BINARY=install_flash_player_23_active_x.msi
 set FLAGS=ALLUSERS=1 /q /norestart
 
 :: Create the log directory if it doesn't exist
@@ -65,7 +65,7 @@ del /F /Q "%SystemDrive%\Windows\tasks\Adobe Flash Player Updater.job" >> "%LOGP
 if exist "%ProgramFiles(x86)%\Adobe\Acrobat 7.0\Distillr\acrotray.exe" (
 	taskkill /im "acrotray.exe" >> "%LOGPATH%\%LOGFILE%" 2>NUL
 	del /f /q "%ProgramFiles(x86)%\Adobe\Acrobat 7.0\Distillr\acrotray.exe" >> "%LOGPATH%\%LOGFILE%" 2>NUL
-	)
+)
 
 :: Pop back to original directory. This isn't necessary in stand-alone runs of the script, but is needed when being called from another script
 popd
