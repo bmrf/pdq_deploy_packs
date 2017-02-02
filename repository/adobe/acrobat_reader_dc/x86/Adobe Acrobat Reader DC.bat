aea1013b3a97de2f6a0174957d3cc751f667bcb5
Vocatus Gate <vocatus.gate@gmail.com>
2017-02-02 11:47:30 -0700

    Add killing of Chrome Acrobat plugin registry entry

--- repository/adobe/acrobat_reader_dc/x86/Adobe Acrobat Reader DC.bat
+++ repository/adobe/acrobat_reader_dc/x86/Adobe Acrobat Reader DC.bat
@@ -1,7 +1,9 @@
 :: Purpose:       Installs a package
 :: Requirements:  Run this script with Administrator rights
 :: Author:        vocatus on reddit.com/r/sysadmin ( vocatus.gate@gmail.com ) // PGP key ID: 0x07d1490f82a211a2
-:: Version:       1.0.1 + Add killing of additional Task Scheduler job
+:: Version:       1.0.3 + Add killing of Chrome Acrobat plugin registry entry
+::                1.0.2 + Add killing of additional Updater registry keys
+::                1.0.1 + Add killing of additional Task Scheduler job
 ::                      + Add killing of Adobe ARM directory
 ::                1.0.0 + Initial write
 
@@ -10,8 +12,8 @@
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set SCRIPT_VERSION=1.0.1
-set SCRIPT_UPDATED=2016-07-10
+set SCRIPT_VERSION=1.0.3
+set SCRIPT_UPDATED=2017-02-02
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
@@ -29,7 +31,7 @@ set LOGFILE=%COMPUTERNAME%_Adobe_Acrobat_DC_install.log
 
 :: Package to install. Do not use trailing slashes (\)
 set BINARY_VERSION=15.007.20033
-set PATCH_VERSION=15.016.20039
+set PATCH_VERSION=15.023.20053
 set FLAGS=ALLUSERS=1 /qn /norestart TRANSFORMS="Adobe Acrobat Reader DC v%BINARY_VERSION%_customizations.mst"
 
 :: Create the log directory if it doesn't exist
@@ -44,6 +46,10 @@ msiexec /i "Adobe Acrobat Reader DC v%BINARY_VERSION%.msi" %FLAGS% >> "%LOGPATH%
 
 :: Install patch
 msiexec /p "Adobe Acrobat Reader DC v%PATCH_VERSION%_patch.msp" REINSTALL=ALL REINSTALLMODE=omus /qn >> "%LOGPATH%\%LOGFILE%" 2>NUL
+
+:: Disable Adobe Updater via registry; both methods
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Adobe\Acrobat Reader\11.0\FeatureLockDown" /v bUpdater /t REG_DWORD /d 00000000 /f >> "%LOGPATH%\%LOGFILE%" 2>NUL
+%SystemRoot%\System32\reg.exe delete "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run" /v "Adobe ARM" /f >> "%LOGPATH%\%LOGFILE%" 2>NUL
 
 :: Delete the Adobe Acrobat Update Service
 net stop AdobeARMservice >> "%LOGPATH%\%LOGFILE%" 2>NUL
@@ -70,7 +76,10 @@ if exist "%ProgramFiles%\common files\adobe\arm" rmdir /s /q "%ProgramFiles%\com
 if exist "%ProgramFiles(x86)%\Adobe\Acrobat 7.0\Distillr\acrotray.exe" (
 	taskkill /im "acrotray.exe"
 	del /f /q "%ProgramFiles(x86)%\Adobe\Acrobat 7.0\Distillr\acrotray.exe" >> "%LOGPATH%\%LOGFILE%" 2>NUL
-	)
+)
+
+:: Delete the stupid Chrome plugin Adobe loads without our consent
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Google\Chrome\Extensions\efaidnbmnnnibpcajpcglclefindmkaj /f >> "%LOGPATH%\%LOGFILE%" 2>NUL
 
 :: Pop back to original directory. This isn't necessary in stand-alone runs of the script, but is needed when being called from another script
 popd
