e46f6f10fca69d28187fc493fa997a6ee37916e4
Vocatus Gate <vocatus.gate@gmail.com>
2017-05-04 06:59:40 -0700

    v1.0.1
    Expand JRE8 mask to catch versions over 99 (3-digit identifier vs. 2). Thanks to /u/flash44007

--- repository/java/jre/8/x86/jre-8-i586.bat
+++ repository/java/jre/8/x86/jre-8-i586.bat
@@ -1,15 +1,16 @@
 :: Purpose:       Installs a package
 :: Requirements:  Run this script with Administrator rights
 :: Author:        vocatus on reddit.com/r/sysadmin ( vocatus.gate@gmail.com ) // PGP key ID: 0x07d1490f82a211a2
-:: History:       1.0.0 + Initial write
+:: History:       1.0.1 * Expand JRE8 mask to catch versions over 99 (3-digit identifier vs. 2). Thanks to /u/flash44007
+::                1.0.0 + Initial write
 
 
 ::::::::::
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set SCRIPT_VERSION=1.0.0
-set SCRIPT_UPDATED=2014-11-18
+set SCRIPT_VERSION=1.0.1
+set SCRIPT_UPDATED=2017-05-04
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
@@ -38,11 +39,14 @@ if not exist %LOGPATH% mkdir %LOGPATH%
 ::::::::::::::::::
 :: INSTALLATION ::
 ::::::::::::::::::
-:: This first removes previous versions of the JRE
+:: This removes previous versions of the JRE
 echo %CUR_DATE% %TIME%   Uninstalling all versions of JRE 8 x86 prior to installation of current version...>> "%LOGPATH%\%LOGFILE%"
-wmic product where "IdentifyingNumber like '{26A24AE4-039D-4CA4-87B4-2F832180__FF}'" call uninstall /nointeractive>> "%LOGPATH%\%LOGFILE%"
+wmic product where "IdentifyingNumber like '{26A24AE4-039D-4CA4-87B4-2F8__180__FF}'" call uninstall /nointeractive >> "%LOGPATH%\%LOGFILE%"
+:: Second version of the command is to catch JRE8 versions above 99 where they start using 3-digit identifiers instead of 2-digit
+wmic product where "IdentifyingNumber like '{26A24AE4-039D-4CA4-87B4-2F__180___FF}'" call uninstall /nointeractive >> "%LOGPATH%\%LOGFILE%"
+
 :: Sometimes the previous line doesn't work for whatever reason, so we run this line as well
-wmic product where "name like 'Java 8 Update __'" uninstall /nointeractive
+wmic product where "name like 'Java 8 Update ___'" uninstall /nointeractive
 
 :: Install the package from a local directory (if all files are in the same directory)
 :: Nothing below this line will log correctly, because MSI logs in a different format than the standard "echo >> %logfile%" commands. Haven't had time to find a workaround.
