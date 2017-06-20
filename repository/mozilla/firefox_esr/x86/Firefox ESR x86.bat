e4b232e538b291f7fdbf6eb06132059ac05b257c
Vocatus Gate <vocatus.gate@gmail.com>
2017-06-20 12:57:45 -0700

    v1.0.2

--- repository/mozilla/firefox_esr/x86/Firefox ESR x86.bat
+++ repository/mozilla/firefox_esr/x86/Firefox ESR x86.bat
@@ -10,8 +10,8 @@
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set SCRIPT_VERSION=1.0.1
-set SCRIPT_UPDATED=2017-08-09
+set SCRIPT_VERSION=1.0.2
+set SCRIPT_UPDATED=2017-06-20
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
