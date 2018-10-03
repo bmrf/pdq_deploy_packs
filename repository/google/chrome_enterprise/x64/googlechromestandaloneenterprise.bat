1cb29cae150036cedc97ccf4cd55d42394a73e79
Vocatus Gate <vocatus.gate@gmail.com>
2018-10-02 17:51:17 -0700

    Add files via upload

--- repository/google/chrome_enterprise/x64/googlechromestandaloneenterprise.bat
+++ repository/google/chrome_enterprise/x64/googlechromestandaloneenterprise.bat
@@ -1,7 +1,8 @@
 :: Purpose:       Installs a package
 :: Requirements:  1. Run this script with Administrator rights
 :: Author:        vocatus on reddit.com/r/sysadmin ( vocatus.gate@gmail.com ) // PGP key ID: 0x07d1490f82a211a2
-:: History:       1.0.1 * Add command line argument to preserve shortcuts, default to False
+:: History:       1.0.2 + Add deletion of additional Google Update scheduled tasks
+::                1.0.1 * Add command line argument to preserve shortcuts, default to False
 ::                1.0.0 + Initial write
 
 
@@ -25,8 +26,8 @@ if not exist %LOGPATH% mkdir %LOGPATH%
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set SCRIPT_VERSION=1.0.1
-set SCRIPT_UPDATED=2018-04-10
+set SCRIPT_VERSION=1.0.2
+set SCRIPT_UPDATED=2018-10-02
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
