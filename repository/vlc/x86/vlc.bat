88404714c79e3c21342c6d67e36958578cdfed7d
Vocatus Gate <vocatus.gate@gmail.com>
2019-05-14 09:46:48 -0600

    uninstall old versions first

--- repository/vlc/x86/vlc.bat
+++ repository/vlc/x86/vlc.bat
@@ -1,15 +1,16 @@
 :: Purpose:       Installs a package
 :: Requirements:  Run this script with Administrator rights
 :: Author:        vocatus on reddit.com/r/sysadmin ( vocatus.gate@gmail.com ) // PGP key ID: 0x07d1490f82a211a2
-:: History:       1.0.0 + Initial write
+:: History:       1.0.1 + Add uninstallation of previous versions prior to installing. Thanks to github:abulgatz
+::                1.0.0 + Initial write
 
 
 ::::::::::
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set SCRIPT_VERSION=1.0.0
-set SCRIPT_UPDATED=2015-07-18
+set SCRIPT_VERSION=1.0.1
+set SCRIPT_UPDATED=2019-05-14
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
@@ -29,7 +30,7 @@ set LOGFILE=
 
 :: Package to install. Do not use trailing slashes (\)
 set LOCATION=
-set BINARY=VLC v2.2.4 x86.exe
+set BINARY=VLC v3.0.6 x86.exe
 set FLAGS=ALLUSERS=1 /L=1033 /S INSTALLDIR="C:\Program Files (x86)\VLC"
 
 :: Create the log directory if it doesn't exist
@@ -38,9 +39,11 @@ if not exist %LOGPATH% mkdir %LOGPATH
 ::::::::::::::::::
 :: INSTALLATION ::
 ::::::::::::::::::
-:: Uninstall VLC
+
+:: Uninstall old versions first
 if exist "%PROGRAMFILES%\VideoLAN\VLC\uninstall.exe" "%PROGRAMFILES%\VideoLAN\VLC\uninstall.exe" /S
 if exist "%PROGRAMFILES(x86)%\VideoLAN\VLC\uninstall.exe" "%PROGRAMFILES(x86)%\VideoLAN\VLC\uninstall.exe" /S
+
 
 :: Install the package from the local folder (if all files are in the same directory)
 "%BINARY%" %FLAGS%
