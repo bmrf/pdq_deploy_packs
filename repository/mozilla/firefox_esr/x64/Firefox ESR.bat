00cfd9827a3f5f06ee7b99539a6694ca87e9fd04
Vocatus Gate <vocatus.gate@gmail.com>
2019-09-17 07:01:01 -0600

    Remove pre-existing autoconfig.js to prevent installation hang. Thanks to u/dimm0k

--- repository/mozilla/firefox_esr/x64/Firefox ESR.bat
+++ repository/mozilla/firefox_esr/x64/Firefox ESR.bat
@@ -1,7 +1,8 @@
 :: Purpose:       Installs a package
 :: Requirements:  Run this script with Administrator rights
 :: Author:        vocatus on reddit.com/r/sysadmin ( vocatus.gate@gmail.com ) // PGP key ID: 0x07d1490f82a211a2
-:: History:       1.0.3 + Add additional uninstall commands make sure we fully remove old versions first. Thanks to github:abulgatz
+:: History:       1.0.4 ! Remove pre-existing autoconfig.js to prevent installation hang. Thanks to u/dimm0k
+::                1.0.3 + Add additional uninstall commands make sure we fully remove old versions first. Thanks to github:abulgatz
 ::                1.0.2 * Add command line argument to preserve shortcuts, default to False
 ::                1.0.1 * Expand Desktop shortcut deletion mask to sweep all subdirectories under the base user profile directory
 ::                1.0.0 + Initial write
@@ -11,8 +12,8 @@
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set SCRIPT_VERSION=1.0.3
-set SCRIPT_UPDATED=2019-05-14
+set SCRIPT_VERSION=1.0.4
+set SCRIPT_UPDATED=2019-09-17
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
@@ -58,7 +59,9 @@ wmic product where "name like 'Mozille Firefox%%'" call uninstall /nointeractive
 :: Install the package from the local folder (if all files are in the same directory)
 "%BINARY%" %FLAGS%
 
-:: Install customizations, built with CCK2
+:: Install customizations, built with CCK2. Remove old config first, if present
+if exist "%ProgramFiles(x86)%\Mozilla Firefox\defaults\pref\autoconfig.js" del /f /s "%ProgramFiles(x86)%\Mozilla Firefox\defaults\pref\autoconfig.js" 2>NUL
+if exist "%ProgramFiles%\Mozilla Firefox\defaults\pref\autoconfig.js" del /f /s "%ProgramFiles%\Mozilla Firefox\defaults\pref\autoconfig.js" 2>NUL
 if exist "%ProgramFiles(x86)%\Mozilla Firefox\" xcopy /s /e /y "autoconfig\*" "%ProgramFiles(x86)%\Mozilla Firefox"
 if exist "%ProgramFiles%\Mozilla Firefox\" xcopy /s /e /y "autoconfig\*" "%ProgramFiles%\Mozilla Firefox"
 
