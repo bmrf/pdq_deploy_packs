e9528f396eb6f17fdc12d3d303618dfc7ae3b8b7
Vocatus Gate <vocatus.gate@gmail.com>
2020-02-05 10:04:09 -0700

    add proper console and logfile logging

--- repository/7-zip/x86/7-Zip x86.bat
+++ repository/7-zip/x86/7-Zip x86.bat
@@ -1,7 +1,8 @@
 :: Purpose:       Installs a package
 :: Requirements:  Run this script with Administrator rights
 :: Author:        vocatus on reddit.com/r/sysadmin ( vocatus.gate@gmail.com ) // PGP key ID: 0x07d1490f82a211a2
-:: History:       1.0.1 - Remove old versions first. Thanks to github:abulgatz
+:: History:       1.0.2 + Add proper console and logfile logging
+::                1.0.1 - Remove old versions first. Thanks to github:abulgatz
 ::                1.0.0 + Initial write
 
 :: Usage:         Run the script and pass one of the following arguments to it:
@@ -18,8 +19,8 @@
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set SCRIPT_VERSION=1.0.1
-set SCRIPT_UPDATED=2019-05-14
+set SCRIPT_VERSION=1.0.2
+set SCRIPT_UPDATED=2020-02-05
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
@@ -48,18 +49,25 @@ pushd "%~dp0"
 :: INSTALLATION ::
 ::::::::::::::::::
 :: Uninstall other versions of 7-zip
+echo %CUR_DATE% %TIME% Removing prior versions, please wait...
+echo %CUR_DATE% %TIME% Removing prior versions, please wait...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 IF EXIST "%ProgramFiles%\7-Zip\Uninstall.exe" "%ProgramFiles%\7-Zip\Uninstall.exe" /S /V"/qn /norestart"
 IF EXIST "%ProgramFiles(x86)%\7-Zip\Uninstall.exe" "%ProgramFiles(x86)%\7-Zip\Uninstall.exe" /S /V"/qn /norestart"
 wmic product where "name like '7-Zip%%'" uninstall /nointeractive
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
 
 :: Install the package from the local folder (if all files are in the same directory)
+echo %CUR_DATE% %TIME% Installing package...
+echo %CUR_DATE% %TIME% Installing package...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 "%BINARY%" %FLAGS%
-
-:: Delete All Users shortcuts
-if exist "%ProgramData%\Microsoft\Windows\Start Menu\Programs\7-Zip" rmdir /s /q "%ProgramData%\Microsoft\Windows\Start Menu\Programs\7-Zip"
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
 
 :: Create file associations
 :: Basically we just use a couple FOR loops to iterate through the list since it's prettier than using individual 'assoc' and 'ftype' commands
+echo %CUR_DATE% %TIME% Creating file associations...
+echo %CUR_DATE% %TIME% Creating file associations...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 if '%1'=='associate_all' goto associate_all
 
 :: This section will run no matter what's passed to the installer, UNLESS it's "associate_all" 
@@ -83,11 +91,5 @@ for %%i in (001,7z,arj,bz2,bzip2,cab,cpio,deb,dmg,fat,gz,gzip,hfs,iso,lha,lzh,lz
 goto finished
 
 :finished
-:: Delete the Start Menu icons
-if exist "%ProgramData%\Microsoft\Windows\Start Menu\Programs\7-Zip" rmdir /s /q "%ProgramData%\Microsoft\Windows\Start Menu\Programs\7-Zip
-
-:: Pop back to original directory. This isn't necessary in stand-alone runs of the script, but is needed when being called from another script
-popd
-
-:: Return exit code to SCCM/PDQ Deploy/etc
-exit /B %EXIT_CODE%
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
