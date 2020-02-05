dad0922ad09a49e98c08ff5b000ea6492c81702b
Vocatus Gate <vocatus.gate@gmail.com>
2020-02-05 10:36:43 -0700

    Update CDBurnerXP.bat

--- repository/impressum/cdburnerxp/x64/CDBurnerXP.bat
+++ repository/impressum/cdburnerxp/x64/CDBurnerXP.bat
@@ -1,15 +1,16 @@
 :: Purpose:       Installs a package
 :: Requirements:  Run this script with Administrator rights
 :: Author:        vocatus on reddit.com/r/sysadmin ( vocatus.gate@gmail.com ) // PGP key ID: 0x07d1490f82a211a2
-:: History:       1.0.0 + Initial write
+:: History:       1.0.1 + Add proper console and logfile logging
+::                1.0.0 + Initial write
 
 
 ::::::::::
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set SCRIPT_VERSION=1.0.0
-set SCRIPT_UPDATED=2014-07-25
+set SCRIPT_VERSION=1.0.1
+set SCRIPT_UPDATED=2020-02-05
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
@@ -23,11 +24,11 @@ cls
 :: VARIABLES :: -- Set these to your desired values
 :::::::::::::::
 :: Log location and name. Do not use trailing slashes (\)
-set LOGPATH=%SystemDrive%\Logs
-set LOGFILE=
+set LOGPATH=%SystemDrive%\logs
+set LOGFILE=%COMPUTERNAME%_Impressum_CDBurnerXP_x64_install.log
 
 :: Package to install. Do not use trailing slashes (\)
-set BINARY=CDBurnerXP v4.5.7.6321 x64.exe
+set BINARY=CDBurnerXP v4.5.8.7128 x64.exe
 set FLAGS=/VERYSILENT /LOADINF=settings.cfg
 
 :: Create the log directory if it doesn't exist
@@ -38,19 +39,33 @@ if not exist %LOGPATH% mkdir %LOGPATH%
 :: INSTALLATION ::
 ::::::::::::::::::
 :: Remove existing installation first
+echo %CUR_DATE% %TIME% Removing previous versions, please wait...
+echo %CUR_DATE% %TIME% Removing previous versions, please wait...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 wmic product where "name like 'CDBurner%%'" uninstall /nointeractive
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
+
 
 :: Install the package from a local directory (if all files are in the same directory)
+echo %CUR_DATE% %TIME% Installing package...
+echo %CUR_DATE% %TIME% Installing package...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 "%BINARY%" %FLAGS%
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
 
 REM This disables the online update check.
 REM In versions 4.2.5 and above, you have to create an INI 
 REM file at the location below with the following contents:
 REM   [Setup]
 REM   DisableOnlineUpdate=1
+echo %CUR_DATE% %TIME% Disabling telemetry and cleaning up...
+echo %CUR_DATE% %TIME% Disabling telemetry and cleaning up...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 if not exist "%ALLUSERSPROFILE%\Canneverbe Limited\CDBurnerXP" mkdir "%ALLUSERSPROFILE%\Canneverbe Limited\CDBurnerXP"
 echo [Setup] > "%ALLUSERSPROFILE%\Canneverbe Limited\CDBurnerXP\Application.ini"
 echo DisableOnlineUpdate=1 >> "%ALLUSERSPROFILE%\Canneverbe Limited\CDBurnerXP\Application.ini"
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
+
 
 :: Pop back to original directory. This isn't necessary in stand-alone runs of the script, but is needed when being called from another script
 popd
