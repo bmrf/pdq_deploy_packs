561af208ac703172776752438416e960cd6707d1
Vocatus Gate <vocatus.gate@gmail.com>
2020-02-05 11:05:15 -0700

    Update Firefox.bat

--- repository/mozilla/firefox/x64/Firefox.bat
+++ repository/mozilla/firefox/x64/Firefox.bat
@@ -1,7 +1,8 @@
 :: Purpose:       Installs a package
 :: Requirements:  Run this script with Administrator rights
 :: Author:        vocatus on reddit.com/r/sysadmin ( vocatus.gate@gmail.com ) // PGP key ID: 0x07d1490f82a211a2
-:: History:       1.0.2 + Add additional uninstall commands make sure we fully remove old versions first. Thanks to github:abulgatz
+:: History:       1.0.3 + Add proper console and logfile logging
+::                1.0.2 + Add additional uninstall commands make sure we fully remove old versions first. Thanks to github:abulgatz
 ::                1.0.1 * Expand Desktop shortcut deletion mask to sweep all subdirectories under the base user profile directory
 ::                1.0.0 + Initial write
 
@@ -10,8 +11,8 @@
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 ::@echo off
-set SCRIPT_VERSION=1.0.2
-set SCRIPT_UPDATED=2019-05-14
+set SCRIPT_VERSION=1.0.3
+set SCRIPT_UPDATED=2020-02-05
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
@@ -26,12 +27,10 @@ cls
 :: VARIABLES :: -- Set these to your desired values
 :::::::::::::::
 :: Log location and name. Do not use trailing slashes (\)
-set LOGPATH=%SystemDrive%\Logs
-set LOGFILE=
+set LOGPATH=%SystemDrive%\logs
+set LOGFILE=%COMPUTERNAME%_Mozilla_Firefox_x64_install.log
 
 :: Package to install. Do not use trailing slashes (\)
-::if     exist "%ProgramFiles(x86)%" set BINARY="Firefox Setup 48.0.0 x64.exe"
-::if not exist "%ProgramFiles(x86)%" set BINARY="Firefox Setup 48.0.0 x86.exe"
 set BINARY=Mozilla Firefox.exe
 set FLAGS=/INI="%CD%\configuration.ini"
 
@@ -43,18 +42,35 @@ if not exist %LOGPATH% mkdir %LOGPATH%
 :: INSTALLATION ::
 ::::::::::::::::::
 :: Kill Firefox first
+echo %CUR_DATE% %TIME% Killing any running Firefox instances, please wait...
+echo %CUR_DATE% %TIME% Killing any running Firefox instances, please wait...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 %SystemDrive%\windows\system32\taskkill.exe /F /IM firefox.exe /T 2>NUL
 wmic process where name="firefox.exe" call terminate 2>NUL
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
+
 
 :: Remove old version first
+echo %CUR_DATE% %TIME% Removing previous versions, please wait...
+echo %CUR_DATE% %TIME% Removing previous versions, please wait...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 IF EXIST "%ProgramFiles%\Mozilla Firefox\uninstall\helper.exe" "%ProgramFiles%\Mozilla Firefox\uninstall\helper.exe" /S
 IF EXIST "%ProgramFiles(x86)%\Mozilla Firefox\uninstall\helper.exe" "%ProgramFiles(x86)%\Mozilla Firefox\uninstall\helper.exe" /S
 wmic product where "name like 'Mozille Firefox%%'" call uninstall /nointeractive
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
+
 
 :: Install the package from the local folder (if all files are in the same directory)
+echo %CUR_DATE% %TIME% Installing package...
+echo %CUR_DATE% %TIME% Installing package...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 "%BINARY%" %FLAGS%
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
+
 
 :: Install customizations, built with CCK2
+echo %CUR_DATE% %TIME% Finishing configuration and cleaning up...
+echo %CUR_DATE% %TIME% Finishing configuration and cleaning up...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 if exist "%ProgramFiles(x86)%\Mozilla Firefox\" xcopy /s /e /y "autoconfig\*" "%ProgramFiles(x86)%\Mozilla Firefox"
 if exist "%ProgramFiles%\Mozilla Firefox\" xcopy /s /e /y "autoconfig\*" "%ProgramFiles%\Mozilla Firefox"
 
@@ -65,6 +81,9 @@ REM if exist "%SystemDrive%\users\default\Desktop\Mozilla Firefox.lnk" del "%Sys
 
 :: Lets just amp this up and catch ANYWHERE it might drop a shortcut 
 del /f /s "%SystemDrive%\Users\*Firefox.lnk" 2>nul
+
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
 
 :: Pop back to original directory. This isn't necessary in stand-alone runs of the script, but is needed when being called from another script
 popd
