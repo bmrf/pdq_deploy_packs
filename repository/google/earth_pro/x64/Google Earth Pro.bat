2972d1cd1e7a3f822ea584b6601e947f5d97c1dd
Vocatus Gate <vocatus.gate@gmail.com>
2020-02-05 10:34:27 -0700

    Update Google Earth Pro.bat

--- repository/google/earth_pro/x64/Google Earth Pro.bat
+++ repository/google/earth_pro/x64/Google Earth Pro.bat
@@ -1,7 +1,8 @@
 :: Purpose:       Installs a package
 :: Requirements:  1. Run this script with Administrator rights
 :: Author:        vocatus on reddit.com/r/sysadmin ( vocatus.gate@gmail.com ) // PGP key ID: 0x07d1490f82a211a2
-:: History:       1.0.2 + Add deletion of additional Google Update scheduled tasks
+:: History:       1.0.3 + Add proper console and logfile logging
+::                1.0.2 + Add deletion of additional Google Update scheduled tasks
 ::                1.0.1 * Add command line argument to preserve shortcuts, default to False
 ::                1.0.0 + Initial write
 
@@ -10,11 +11,11 @@
 :: VARIABLES :: -- Set these to your desired values
 :::::::::::::::
 :: Log location and name. Do not use trailing slashes (\)
-set LOGPATH=%SystemDrive%\Logs
+set LOGPATH=%SystemDrive%\logs
 set LOGFILE=
 
 :: Package to install. Do not use trailing slashes (\)
-set BINARY_VERSION=Google Earth Pro v7.3.2.exe
+set BINARY=Google Earth Pro v7.3.2.exe
 set FLAGS=OMAHA=1
 
 :: Create the log directory if it doesn't exist
@@ -25,8 +26,8 @@ if not exist %LOGPATH% mkdir %LOGPATH%
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set SCRIPT_VERSION=1.0.0
-set SCRIPT_UPDATED=2019-01-28
+set SCRIPT_VERSION=1.0.3
+set SCRIPT_UPDATED=2020-02-05
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
@@ -44,8 +45,15 @@ cls
 :: INSTALLATION ::
 ::::::::::::::::::
 :: Install package from local directory (if all files are in the same directory)
-"%BINARY_VERSION%" %FLAGS%
+echo %CUR_DATE% %TIME% Installing package...
+echo %CUR_DATE% %TIME% Installing package...>> "%LOGPATH%\%LOGFILE%" 2>NUL
+"%BINARY%" %FLAGS%
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
 
+
+echo %CUR_DATE% %TIME% Disabling telemetry and cleaning up...
+echo %CUR_DATE% %TIME% Disabling telemetry and cleaning up...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 :: Import the reg file that disables Chrome auto-updater
 regedit /s Tweak_Disable_Chrome_Auto-Update.reg
 
@@ -73,6 +81,9 @@ if %PRESERVE_SHORTCUTS%==no (
 	:: Windows 7
 	if exist "%public%\Desktop\Google Earth Pro.lnk" del "%public%\Desktop\Google Earth Pro.lnk"
 )
+
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
 
 :: Pop back to original directory. Isn't necessary in stand-alone runs of the script, but is needed when being called from another script
 popd
