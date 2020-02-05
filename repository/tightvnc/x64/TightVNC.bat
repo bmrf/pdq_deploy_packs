b838b9152280c26664e866b07f5da0d94bba529c
Vocatus Gate <vocatus.gate@gmail.com>
2020-02-05 11:19:39 -0700

    Update TightVNC.bat

--- repository/tightvnc/x64/TightVNC.bat
+++ repository/tightvnc/x64/TightVNC.bat
@@ -1,7 +1,8 @@
 :: Purpose:       Installs a package
 :: Requirements:  Run this script with Administrator rights
 :: Author:        vocatus on reddit.com/r/sysadmin ( vocatus.gate@gmail.com ) // PGP key ID: 0x07d1490f82a211a2
-:: History:       1.0.1 + Add commands to stop and restart TightVNC server service after installation. Thanks to /u/BadMoodinTheMorning
+:: History:       1.0.2 + Add proper console and logfile logging
+::                1.0.1 + Add commands to stop and restart TightVNC server service after installation. Thanks to /u/BadMoodinTheMorning
 ::                1.0.0 + Initial write
 
 
@@ -9,8 +10,8 @@
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set SCRIPT_VERSION=1.0.1
-set SCRIPT_UPDATED=2017-02-15
+set SCRIPT_VERSION=1.0.2
+set SCRIPT_UPDATED=2020-02-05
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
@@ -24,12 +25,11 @@ cls
 :: VARIABLES :: -- Set these to your desired values
 :::::::::::::::
 :: Log location and name. Do not use trailing slashes (\)
-set LOGPATH=%SystemDrive%\Logs
-set LOGFILE=
+set LOGPATH=%SystemDrive%\logs
+set LOGFILE=%COMPUTERNAME%_TightVNC_x64_install.log
 
 :: Package to install. Do not use trailing slashes (\)
-set LOCATION=
-set BINARY=TightVNC v2.8.5 x64.msi
+set BINARY=TightVNC v2.8.27 x64.msi
 set FLAGS=/quiet /norestart ADDLOCAL="Server,Viewer"
 
 :: Create the log directory if it doesn't exist
@@ -40,37 +40,66 @@ if not exist %LOGPATH% mkdir %LOGPATH%
 :: INSTALLATION ::
 ::::::::::::::::::
 :: This line uninstalls any prior version
+echo %CUR_DATE% %TIME% Removing previous versions, please wait...
+echo %CUR_DATE% %TIME% Removing previous versions, please wait...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 "%ProgramFiles%\TightVNC\uninstall.exe" /S >NUL
 "%ProgramFiles(x86)%\TightVNC\uninstall.exe" /S >NUL
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
+
 
 :: Stop any running servers
+echo %CUR_DATE% %TIME% Stopping any running servers...
+echo %CUR_DATE% %TIME% Stopping any running servers...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 "%ProgramFiles%\TightVNC\tvnserver.exe" -stop -silent
 
 :: Delay to let it finish
 ping 127.0.0.1 -n 2
 
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
+
+
 :: Install the new version
+echo %CUR_DATE% %TIME% Installing package...
+echo %CUR_DATE% %TIME% Installing package...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 msiexec.exe /i "%BINARY%" %FLAGS%
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
+
 
 :: Install the new server/listener, and then stops it so we can import the settings file
+echo %CUR_DATE% %TIME% Loading configuration files...
+echo %CUR_DATE% %TIME% Loading configuration files...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 "%ProgramFiles%\TightVNC\tvnserver.exe" -stop -silent
 "%ProgramFiles%\TightVNC\tvnserver.exe" -install -silent
 "%ProgramFiles%\TightVNC\tvnserver.exe" -stop -silent
 
 :: This line imports the new settings
 regedit /s "TightVNC settings.reg"
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
+
 
 :: This line starts the server back up
-"%ProgramFiles%\TightVNC\tvnserver.exe" -start -silent
+echo %CUR_DATE% %TIME% Starting VNC server back up...
+echo %CUR_DATE% %TIME% Starting VNC server back up...>> "%LOGPATH%\%LOGFILE%" 2>NUL
+"%ProgramFiles%\TightVNC\tvnserver.exe" -start -silent >> "%LOGPATH%\%LOGFILE%" 2>NUL
 
 :: Additional step to make sure the service is running
 net stop tvnserver
 net start tvnserver
 
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
+
 
 :::::::::::::
 :: CLEANUP ::
 :::::::::::::
+echo %CUR_DATE% %TIME% Cleaning up...
+echo %CUR_DATE% %TIME% Cleaning up...>> "%LOGPATH%\%LOGFILE%" 2>NUL
+
 :: 64-bit
 if exist "%ProgramFiles%\TightVNC\TightVNC Web Site.url" del "%ProgramFiles%\TightVNC\TightVNC Web Site.url"
 if exist "%ProgramFiles%\TightVNC\LICENSE.txt" del "%ProgramFiles%\TightVNC\LICENSE.txt"
@@ -82,6 +111,9 @@ if exist "%ProgramFiles(x86)%\TightVNC\LICENSE.txt" del "%ProgramFiles(x86)%\Tig
 :: Start menu shortcuts. Comment these lines out if you want to keep start menu shortcuts
 if exist "%ProgramData%\Microsoft\Windows\Start Menu\Programs\TightVNC\" rmdir /s /q "%ProgramData%\Microsoft\Windows\Start Menu\Programs\TightVNC\"
 if exist "%AllUsersProfile%\Start Menu\Programs\TightVNC\" rmdir /s /q "%AllUsersProfile%\Start Menu\Programs\TightVNC\"
+
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
 
 :: Pop back to original directory. This isn't necessary in stand-alone runs of the script, but is needed when being called from another script
 popd
