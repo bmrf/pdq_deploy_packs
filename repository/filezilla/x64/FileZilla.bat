0b780d589bfd0b9a7f896972f1630ec8bd662f21
Vocatus Gate <vocatus.gate@gmail.com>
2020-02-05 10:23:52 -0700

    Add proper console and logfile logging

--- repository/filezilla/x64/FileZilla.bat
+++ repository/filezilla/x64/FileZilla.bat
@@ -1,15 +1,16 @@
 :: Purpose:       Installs a package
 :: Requirements:  Run this script with Administrator rights
 :: Author:        vocatus on reddit.com/r/sysadmin ( vocatus.gate@gmail.com ) // PGP key ID: 0x07d1490f82a211a2
-:: History:       1.0.0 + Initial write
+:: History:       1.0.1 + Add proper console and logfile logging
+::                1.0.0 + Initial write
 
 
 ::::::::::
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set SCRIPT_VERSION=1.0.0
-set SCRIPT_UPDATED=2015-04-15
+set SCRIPT_VERSION=1.0.1
+set SCRIPT_UPDATED=2020-02-05
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
@@ -24,11 +25,10 @@ cls
 :::::::::::::::
 :: Log location and name. Do not use trailing slashes (\)
 set LOGPATH=%SystemDrive%\Logs
-set LOGFILE=
+set LOGFILE=%COMPUTERNAME%_FileZilla_x64_install.log
 
 :: Package to install. Do not use trailing slashes (\)
-set LOCATION=
-set BINARY=FileZilla v3.20.1 x64.exe
+set BINARY=FileZilla v3.46.3.exe
 set FLAGS=/S
 
 :: Create the log directory if it doesn't exist
@@ -39,14 +39,28 @@ if not exist %LOGPATH% mkdir %LOGPATH%
 :: INSTALLATION ::
 ::::::::::::::::::
 :: Remove previous versions first
+echo %CUR_DATE% %TIME% Removing previous versions, please wait...
+echo %CUR_DATE% %TIME% Removing previous versions, please wait...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 if exist "%ProgramFiles(x86)%\FileZilla FTP Client\uninstall.exe" "%ProgramFiles(x86)%\FileZilla FTP Client\uninstall.exe" /S 2>NUL
 if exist "%ProgramFiles%\FileZilla FTP Client\uninstall.exe" "%ProgramFiles%\FileZilla FTP Client\uninstall.exe" /S 2>NUL
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
+
 
 :: Install the package from a local directory (if all files are in the same directory)
+echo %CUR_DATE% %TIME% Installing package...
+echo %CUR_DATE% %TIME% Installing package...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 "%BINARY%" %FLAGS%
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
+
 
 :: Remove FileZilla Desktop Icon
+echo %CUR_DATE% %TIME% Cleaning up...
+echo %CUR_DATE% %TIME% Cleaning up...>> "%LOGPATH%\%LOGFILE%" 2>NUL
 if exist "%public%\Desktop\FileZilla Client.lnk" del "%public%\Desktop\FileZilla Client.lnk"
+echo %CUR_DATE% %TIME% Done.
+echo %CUR_DATE% %TIME% Done.>> "%LOGPATH%\%LOGFILE%" 2>NUL
 
 :: Pop back to original directory. This isn't necessary in stand-alone runs of the script, but is needed when being called from another script
 popd
